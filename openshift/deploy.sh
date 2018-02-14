d230130aee764434f7a0072d9bcaa0297ac9cab1
Fridolin Pokorny <fridolin@redhat.com>
2018-02-14 14:43:08 +0100

    Uncomment deployment of thoth-core

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-#oc cluster up
-#oc new-project thoth-frontend
-#oc new-project thoth-middleend
-#oc process -f template.yaml | oc apply -f -
-#oc project thoth-frontend
+oc cluster up
+oc new-project thoth-frontend
+oc new-project thoth-middleend
+oc process -f template.yaml | oc apply -f -
+oc project thoth-frontend
 
 # Deploy Gremlin for a local setup
 [ "$THOTH_LOCAL" = "1" ] && oc process -f gremlin.yaml | oc apply -f -
