e6524b121ade0720509b69677657d3669bc8fc7d
Fridolin Pokorny <fridolin@redhat.com>
2018-02-19 07:12:07 +0100

    Fix issuing local deployment

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -ex
+set -x
 
-oc cluster up
+#oc cluster up
 oc new-project thoth-frontend
 oc new-project thoth-middleend
 oc process -f template.yaml | oc apply -f -
 oc project thoth-frontend
 
 # Deploy Gremlin for a local setup
-[ "$THOTH_LOCAL" = "1" ] && oc process -f gremlin.yaml | oc apply -f -
+[ "${THOTH_LOCAL}" = 1 ] && oc process -f gremlin.yaml | oc apply -f -
