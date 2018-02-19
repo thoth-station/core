413dd3de15557fe0d19b894439e02c90c17add1c
Fridolin Pokorny <fridolin@redhat.com>
2018-02-19 07:18:41 +0100

    Deploy gremlin to middleend

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -6,7 +6,7 @@ set -x
 oc new-project thoth-frontend
 oc new-project thoth-middleend
 oc process -f template.yaml | oc apply -f -
-oc project thoth-frontend
 
 # Deploy Gremlin for a local setup
+oc project thoth-middleend
 [ "${THOTH_LOCAL}" = 1 ] && oc process -f gremlin.yaml | oc apply -f -
