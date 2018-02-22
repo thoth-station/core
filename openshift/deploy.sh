58aa14361862014fb15aa3c8a4af00d4a5cd3b8e
Fridolin Pokorny <fridolin@redhat.com>
2018-02-22 09:52:51 +0100

    Introduce thoth-backend

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -5,6 +5,7 @@ set -x
 #oc cluster up
 oc new-project thoth-frontend
 oc new-project thoth-middleend
+oc new-project thoth-backend
 oc process -f template.yaml | oc apply -f -
 
 # Deploy Gremlin for a local setup
