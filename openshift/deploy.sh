3128ff5752c32518dfd0b70d978ce32d0eb49596
Fridolin Pokorny <fridolin@redhat.com>
2018-02-26 15:03:28 +0100

    Add parameters for docker IP for services

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -2,11 +2,13 @@
 
 set -x
 
-#oc cluster up
+DOCKER_IP=`ip a | grep inet |grep docker | head -1 | sed 's#.* \([^/]*\)/.*#\1#'`
+
+oc cluster up --routing-suffix="${DOCKER_IP}.nip.io"
 oc new-project thoth-frontend
 oc new-project thoth-middleend
 oc new-project thoth-backend
-oc process -f template.yaml | oc apply -f -
+oc process -f template.yaml -p DOCKER_IP=${DOCKER_IP} | oc apply -f -
 
 # Deploy Gremlin for a local setup
 oc project thoth-middleend
