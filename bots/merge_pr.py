95b50cd51b67613028731510d6574d41c921f692
Christoph Görn <goern@redhat.com>
2018-06-12 13:48:39 +0200

    sending metrics to prometheus push gateway
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- bots/merge_pr.py
+++ bots/merge_pr.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# thoth-core
+# Copyright(C) 2018 Christoph Görn
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>./env python3
+
+
+"""This will merge PR"""
 
 import os
 import logging
@@ -12,6 +31,8 @@ from github import Github
 from github import UnknownObjectException
 from github.GithubException import GithubException
 
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+
 from sesheta.common import CICD_CONTEXT_ID, DO_NOT_MERGE_LABELS, get_labels_from_pr, commit_was_successful_tested, init_github_interface
 
 
@@ -20,14 +41,19 @@ SESHETA_GITHUB_ACCESS_TOKEN = os.getenv('SESHETA_GITHUB_ACCESS_TOKEN', None)
 SERVER_URL = os.getenv('SESHETA_SERVER_URL', 'https://chat.openshift.io')
 TEAM_ID = os.getenv('SESHETA_TEAM_ID', 'aicoe')
 ENDPOINT_URL = os.getenv('SESHETA_ENDPOINT_URL', None)
+PROMETHEUS_PUSH_GATEWAY = os.getenv('PROMETHEUS_PUSH_GATEWAY', 'pushgateway:9091')
+
+prometheus_registry = CollectorRegistry()
+_METRIC_MERGED_PULLREQUESTS = Gauge(
+    'pullrequests_merged_total', 'total number of Pull Requests merged', registry=prometheus_registry)
 
 daiquiri.setup(level=logging.INFO)
-logger = daiquiri.getLogger('merger')
+_LOGGER = daiquiri.getLogger('merger')
 
 if DEBUG:
-    logger.setLevel(level=logging.DEBUG)
+    _LOGGER.setLevel(level=logging.DEBUG)
 else:
-    logger.setLevel(level=logging.INFO)
+    _LOGGER.setLevel(level=logging.INFO)
 
 
 class InvalidPayload(Exception):
@@ -48,7 +74,7 @@ def notify_channel(message):
 
 if __name__ == '__main__':
     if not SESHETA_GITHUB_ACCESS_TOKEN:
-        logger.error(
+        _LOGGER.error(
             'Github Token not provided via environment variable SESHETA_GITHUB_ACCESS_TOKEN')
         exit(-1)
 
@@ -59,12 +85,12 @@ if __name__ == '__main__':
     github, org, GITHUB_ORGANIZATION, GITHUB_REPOSITORIES, DEFAULT_LABELS = init_github_interface(
         SESHETA_GITHUB_ACCESS_TOKEN)
 
-    logger.info(
+    _LOGGER.info(
         f"Hi, I'm {github.get_user().name}, and I'm fully operational now!")
-    logger.debug("... and I am running in DEBUG mode!")
+    _LOGGER.debug("... and I am running in DEBUG mode!")
 
     for _repo in GITHUB_REPOSITORIES:
-        logger.info(
+        _LOGGER.info(
             f"checking Repository '{org.login}/{_repo}' for 'approved' Pull Requests")
         repo = org.get_repo(_repo)
 
@@ -73,13 +99,13 @@ if __name__ == '__main__':
                 merge_it = False
                 labels = []
 
-                logger.debug(
+                _LOGGER.debug(
                     f"check if conditions to merge '{pr.title}' are met")
 
                 try:
                     labels = pr.as_issue().get_labels()
                 except AttributeError as e:
-                    logger.error(e)
+                    _LOGGER.error(e)
 
                 for label in labels:
                     if label.name == 'approved':
@@ -90,17 +116,28 @@ if __name__ == '__main__':
 
                 if merge_it:
                     try:
-                        logger.info(
+                        _LOGGER.info(
                             f"I am going to merge Pull Request '{pr.title}'... let's do it!")
 
                         merged = pr.merge()
 
-                        logger.info(f"Pull Request '{pr.title}' {merged.message}")
+                        _LOGGER.info(
+                            f"Pull Request '{pr.title}' {merged.message}")
+
+                        _METRIC_MERGED_PULLREQUESTS.inc()
 
                         notify_channel(
                             f"Pull Request <a herf='{pr.html_url}'>{pr.title}</a> has been successfully merged :tada:")
 
                     except HTTPError as e:
-                        logger.error(e)
+                        _LOGGER.error(e)
                     except GithubException as e:
-                        logger.error(e)
+                        _LOGGER.error(e)
+
+    if PROMETHEUS_PUSH_GATEWAY:
+        try:
+            push_to_gateway(PROMETHEUS_PUSH_GATEWAY, job='sesheta-merge-pr',
+                            registry=prometheus_registry)
+        except Exception as e:
+            _LOGGER.exception(
+                'An error occurred pushing the metrics: {}'.format(str(e)))
