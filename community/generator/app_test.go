595acfd5eed9d2a36fca1650bf4c44595e6f6f74
Harshad Reddy Nalla <hnalla@redhat.com>
2021-04-15 21:39:56 -0400

    :zap: pre-commit fixes on all the files
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- community/generator/app_test.go
+++ community/generator/app_test.go
@@ -17,74 +17,74 @@ limitations under the License.
 package main
 
 import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
-	"testing"
+    "io/ioutil"
+    "os"
+    "path/filepath"
+    "strings"
+    "testing"
 )
 
 func TestNonExistentDirIsCreated(t *testing.T) {
-	dir := "/tmp/nonexistent"
-	err := createDirIfNotExists(dir)
-	if err != nil {
-		t.Fatalf("Received error creating dir: %v", err)
-	}
-	if !pathExists(dir) {
-		t.Fatalf("%s should exist", dir)
-	}
+    dir := "/tmp/nonexistent"
+    err := createDirIfNotExists(dir)
+    if err != nil {
+        t.Fatalf("Received error creating dir: %v", err)
+    }
+    if !pathExists(dir) {
+        t.Fatalf("%s should exist", dir)
+    }
 }
 
 func TestExistentDirNotCreated(t *testing.T) {
-	dir := "./testdata"
-	err := createDirIfNotExists(dir)
-	if err != nil {
-		t.Fatalf("Received error creating dir: %v", err)
-	}
+    dir := "./testdata"
+    err := createDirIfNotExists(dir)
+    if err != nil {
+        t.Fatalf("Received error creating dir: %v", err)
+    }
 }
 
 func TestGetExistingData(t *testing.T) {
-	cases := []struct {
-		path      string
-		expected  string
-		expectErr bool
-	}{
-		{
-			path:      "./testdata/custom_content.md",
-			expected:  "FOO BAR BAZ",
-			expectErr: false,
-		},
-		{
-			path:      "./testdata/no_custom_content.md",
-			expected:  "",
-			expectErr: false,
-		},
-		{
-			path:      "./testdata/foo.md",
-			expected:  "",
-			expectErr: true,
-		},
-	}
+    cases := []struct {
+        path      string
+        expected  string
+        expectErr bool
+    }{
+        {
+            path:      "./testdata/custom_content.md",
+            expected:  "FOO BAR BAZ",
+            expectErr: false,
+        },
+        {
+            path:      "./testdata/no_custom_content.md",
+            expected:  "",
+            expectErr: false,
+        },
+        {
+            path:      "./testdata/foo.md",
+            expected:  "",
+            expectErr: true,
+        },
+    }
 
-	for _, c := range cases {
-		content, err := getExistingContent(c.path, "markdown")
-		if err != nil && c.expectErr == false {
-			t.Fatalf("Received unexpected error for %s: %v", c.path, err)
-		}
-		if err == nil && c.expectErr == true {
-			t.Fatalf("Expected error for %s but received none", c.path)
-		}
-		if content != c.expected {
-			t.Fatalf("Expected %s but received %s", c.expected, content)
-		}
-	}
+    for _, c := range cases {
+        content, err := getExistingContent(c.path, "markdown")
+        if err != nil && c.expectErr == false {
+            t.Fatalf("Received unexpected error for %s: %v", c.path, err)
+        }
+        if err == nil && c.expectErr == true {
+            t.Fatalf("Expected error for %s but received none", c.path)
+        }
+        if content != c.expected {
+            t.Fatalf("Expected %s but received %s", c.expected, content)
+        }
+    }
 }
 
 func TestWriteTemplate(t *testing.T) {
-	baseGeneratorDir = "generated"
-	templateDir = "../../generator"
+    baseGeneratorDir = "generated"
+    templateDir = "../../generator"
 
-	customContent := `
+    customContent := `
 <!-- BEGIN CUSTOM CONTENT -->
 Example
 custom
@@ -93,143 +93,143 @@ content!
 <!-- END CUSTOM CONTENT -->
 `
 
-	cases := []struct {
-		templatePath string
-		outputPath   string
-		data         map[string]string
-		expectErr    bool
-		fileFormat   string
-		expected     string
-	}{
-		{
-			templatePath: "./testdata/non_existent_template.tmpl",
-			expectErr:    true,
-			fileFormat:   "markdown",
-		},
-		{
-			templatePath: "./testdata/example.tmpl",
-			outputPath:   "/tmp/non_existing_path.md",
-			expectErr:    false,
-			fileFormat:   "markdown",
-			data:         map[string]string{"Message": "Hello!"},
-			expected:     "Hello!",
-		},
-		{
-			templatePath: "./testdata/example.tmpl",
-			outputPath:   "./testdata/example.md",
-			expectErr:    false,
-			fileFormat:   "markdown",
-			data:         map[string]string{"Message": "Hello!"},
-			expected:     customContent,
-		},
-	}
+    cases := []struct {
+        templatePath string
+        outputPath   string
+        data         map[string]string
+        expectErr    bool
+        fileFormat   string
+        expected     string
+    }{
+        {
+            templatePath: "./testdata/non_existent_template.tmpl",
+            expectErr:    true,
+            fileFormat:   "markdown",
+        },
+        {
+            templatePath: "./testdata/example.tmpl",
+            outputPath:   "/tmp/non_existing_path.md",
+            expectErr:    false,
+            fileFormat:   "markdown",
+            data:         map[string]string{"Message": "Hello!"},
+            expected:     "Hello!",
+        },
+        {
+            templatePath: "./testdata/example.tmpl",
+            outputPath:   "./testdata/example.md",
+            expectErr:    false,
+            fileFormat:   "markdown",
+            data:         map[string]string{"Message": "Hello!"},
+            expected:     customContent,
+        },
+    }
 
-	for _, c := range cases {
-		err := writeTemplate(c.templatePath, c.outputPath, c.fileFormat, c.data)
-		if err != nil && c.expectErr == false {
-			t.Fatalf("Received unexpected error for %s: %v", c.templatePath, err)
-		}
-		if c.expectErr {
-			if err == nil {
-				t.Fatalf("Expected error for %s but received none", c.templatePath)
-			}
-			continue
-		}
-		content, err := ioutil.ReadFile(c.outputPath)
-		if err != nil {
-			t.Fatalf("%s should exist", c.outputPath)
-		}
-		if strings.Contains(string(content), c.expected) == false {
-			t.Fatalf("%s was not found in %s", c.expected, c.outputPath)
-		}
-	}
+    for _, c := range cases {
+        err := writeTemplate(c.templatePath, c.outputPath, c.fileFormat, c.data)
+        if err != nil && c.expectErr == false {
+            t.Fatalf("Received unexpected error for %s: %v", c.templatePath, err)
+        }
+        if c.expectErr {
+            if err == nil {
+                t.Fatalf("Expected error for %s but received none", c.templatePath)
+            }
+            continue
+        }
+        content, err := ioutil.ReadFile(c.outputPath)
+        if err != nil {
+            t.Fatalf("%s should exist", c.outputPath)
+        }
+        if strings.Contains(string(content), c.expected) == false {
+            t.Fatalf("%s was not found in %s", c.expected, c.outputPath)
+        }
+    }
 }
 
 func TestGroupDirName(t *testing.T) {
-	group := Group{Name: "Foo Bar"}
-	if group.DirName("sig") != "sig-foo-bar" {
-		t.Fatal("DirName incorrect")
-	}
+    group := Group{Name: "Foo Bar"}
+    if group.DirName("sig") != "sig-foo-bar" {
+        t.Fatal("DirName incorrect")
+    }
 }
 
 func TestCreateGroupReadmes(t *testing.T) {
-	baseGeneratorDir = "generated"
-	templateDir = "../../generator"
+    baseGeneratorDir = "generated"
+    templateDir = "../../generator"
 
-	groups := []Group{
-		{Name: "Foo"},
-		{Name: "Bar"},
-	}
+    groups := []Group{
+        {Name: "Foo"},
+        {Name: "Bar"},
+    }
 
-	err := createGroupReadme(groups, "sig")
-	if err != nil {
-		t.Fatal(err)
-	}
+    err := createGroupReadme(groups, "sig")
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	for _, group := range groups {
-		path := filepath.Join(baseGeneratorDir, group.DirName("sig"), "README.md")
-		if !pathExists(path) {
-			t.Fatalf("%s should exist", path)
-		}
-	}
+    for _, group := range groups {
+        path := filepath.Join(baseGeneratorDir, group.DirName("sig"), "README.md")
+        if !pathExists(path) {
+            t.Fatalf("%s should exist", path)
+        }
+    }
 }
 
 func TestReadmesAreSkipped(t *testing.T) {
-	baseGeneratorDir = "generated"
-	templateDir = "../../generator"
+    baseGeneratorDir = "generated"
+    templateDir = "../../generator"
 
-	os.Setenv("SIG", "sig-foo")
+    os.Setenv("SIG", "sig-foo")
 
-	groups := []Group{
-		{Name: "Foo"},
-		{Name: "Bar"},
-	}
+    groups := []Group{
+        {Name: "Foo"},
+        {Name: "Bar"},
+    }
 
-	err := createGroupReadme(groups, "sig")
-	if err != nil {
-		t.Fatal(err)
-	}
+    err := createGroupReadme(groups, "sig")
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	for _, group := range groups[1:] {
-		path := filepath.Join(baseGeneratorDir, group.DirName("sig"), "README.md")
-		if !pathExists(path) {
-			t.Fatalf("%s should exist", path)
-		}
-	}
+    for _, group := range groups[1:] {
+        path := filepath.Join(baseGeneratorDir, group.DirName("sig"), "README.md")
+        if !pathExists(path) {
+            t.Fatalf("%s should exist", path)
+        }
+    }
 
-	os.Setenv("SIG", "")
+    os.Setenv("SIG", "")
 }
 
 func copyFile(src, dst string) error {
-	// Read all content of src to data
-	data, err := ioutil.ReadFile(src)
-	if err != nil {
-		return err
-	}
-	// Write data to dst
-	err = ioutil.WriteFile(dst, data, 0644)
-	if err != nil {
-		return err
-	}
-	return nil
+    // Read all content of src to data
+    data, err := ioutil.ReadFile(src)
+    if err != nil {
+        return err
+    }
+    // Write data to dst
+    err = ioutil.WriteFile(dst, data, 0644)
+    if err != nil {
+        return err
+    }
+    return nil
 }
 
 func TestFullGeneration(t *testing.T) {
-	baseGeneratorDir = "generated"
-	templateDir = "../../generator"
+    baseGeneratorDir = "generated"
+    templateDir = "../../generator"
 
-	err := copyFile("testdata/sigs.yaml", "generated/sigs.yaml")
-	if err != nil {
-		t.Fatalf("Error received: %v", err)
-	}
+    err := copyFile("testdata/sigs.yaml", "generated/sigs.yaml")
+    if err != nil {
+        t.Fatalf("Error received: %v", err)
+    }
 
-	main()
+    main()
 
-	expectedDirs := []string{"sig-foo", "sig-bar", "wg-baz"}
-	for _, ed := range expectedDirs {
-		path := filepath.Join(baseGeneratorDir, ed, "README.md")
-		if !pathExists(path) {
-			t.Fatalf("%s should exist", path)
-		}
-	}
+    expectedDirs := []string{"sig-foo", "sig-bar", "wg-baz"}
+    for _, ed := range expectedDirs {
+        path := filepath.Join(baseGeneratorDir, ed, "README.md")
+        if !pathExists(path) {
+            t.Fatalf("%s should exist", path)
+        }
+    }
 }
